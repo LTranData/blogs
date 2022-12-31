72a3f618d97874631208909f4a23f97890ec3799
lam1051999 <lam1051999.com>
2023-01-01 00:10:05 +0700

    add features

--- src/theme/Footer.tsx
+++ src/theme/Footer.tsx
@@ -12,26 +12,34 @@ function Footer(props) {
           }}
         >
           <div style={{ maxWidth: 650 }}>
-            I believe each of us was designed to do certain things, that we have certain duties. Most significantly, since nature intended us to be social creatures, we have duties to our fellow men.
+            I am a highly motivated and passionate data engineer. You'll mostly
+            find me doing some engineering stuff in the big data ecosystem. I
+            often work with Python, Scala, and Java and use the latest big data
+            technologies to solve problems and making tools to improve mine and
+            others work productivity.
           </div>
-          <div style={{marginLeft: 30, minWidth: 169}}>
+          <div style={{ marginLeft: 30, minWidth: 169 }}>
             <img
               src="/blogs/img/avatar.jpg"
               style={{
                 height: 154,
-                width: 139
+                width: 139,
               }}
             />
-            <p style={{fontSize: "0.8em", marginTop: "1em"}}>Tran Lam<br/>Data Engineer</p>
+            <p style={{ fontSize: "0.8em", marginTop: "1em" }}>
+              Tran Lam
+              <br />
+              Data Engineer
+            </p>
           </div>
         </div>
       </div>
       <div
-          style={{
-            display: "flex",
-            justifyContent: "center",
-          }}
-        >
+        style={{
+          display: "flex",
+          justifyContent: "center",
+        }}
+      >
         <OriginalFooter {...props} />
       </div>
     </div>
