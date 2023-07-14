5b18a6ff96747372b931c02d793d85ad367fc11f
lam1051999 <lam1051999.com>
2023-07-14 12:44:06 +0700

    add feature

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,7 +15,7 @@ const config = {
   baseUrl: `/`,
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
-  favicon: `/${process.env.PROJECT_NAME}/img/lt_logo.svg`,
+  favicon: `/img/lt_logo.svg`,
   organizationName: process.env.ORGANIZATION_NAME,
   projectName: `${process.env.PROJECT_NAME}`,
 
